1c5c3a17ed5fa63d6c22ac6f82c19ab40ec1732c
lehahung2k <hung2000yt@gmail.com>
2022-08-19 15:40:39 +0700

    update models

--- server/server.js
+++ server/server.js
@@ -4,20 +4,23 @@ const mysql = require('mysql');
 const app = express();
 const port = 8080;
 
+const db = require("./models");
+
 app.use(express.json());
 
-const db = mysql.createConnection({
-    user: "eventmgnt",
-    host: "mycel.app",
-    port: "3306",
-    password: "hung22hieu",
-    database: "eventmgnt"
+// const db = mysql.createConnection({
+//     user: "eventmgnt",
+//     host: "mycel.app",
+//     port: "3306",
+//     password: "hung22hieu",
+//     database: "eventmgnt"
+// });
+db.sequelize.sync().then(() => {
+    app.listen(port, () => {
+        console.log("Hello NodeJS");
+    });
 });
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
-
-app.listen(port, () => {
-    console.log("Hello NodeJS");
 })
