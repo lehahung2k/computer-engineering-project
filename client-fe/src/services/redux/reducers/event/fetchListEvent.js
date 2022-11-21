f779107e94bdd4416f7006deaed9d0eb841dfd49
Vu Dinh Hieu <hieuvudinh409@gmail.com>
2022-11-21 15:16:20 +0700

    fea: fetch list event redux-thunk

---
+++ client-fe/src/services/redux/reducers/event/fetchListEvent.js
@@ -0,0 +1,37 @@
+const initialState = {
+  loading: false,
+  success: false,
+  data: [],
+};
+
+export const fetchListEvent = (state = initialState, action) => {
+  switch (action.type) {
+    case "EVENT/FETCH_LIST_EVENT": {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+
+    case "EVENT/FETCH_LIST_EVENT_SUCCESS": {
+      const listEvent = action.payload;
+      return {
+        ...state,
+        loading: false,
+        success: true,
+        data: listEvent,
+      };
+    }
+
+    case "EVENT/FETCH_LIST_EVENT_FAIL": {
+      return {
+        ...state,
+        loading: false,
+        success: false,
+        message: action.message,
+      };
+    }
+    default:
+      return state;
+  }
+};
